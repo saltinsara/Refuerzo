d1452e476bd029cfeab0810db36718e1d68546ce
saltinsara <saraax732@gmail.com>
2020-11-27 22:22:19 -0500

    subiendo refuerzo de Laura

---
+++ src/Paginas/Rick.js
@@ -0,0 +1,365 @@
+import React from "react";
+import "../Componentes/compenents.css";
+import rick from "../Imagenes/rick.png";
+import morty from "../Imagenes/morty.png";
+import summer from "../Imagenes/summer.png";
+import beth from "../Imagenes/Beth.png";
+import jerry from "../Imagenes/jerry.png";
+import squanchy from "../Imagenes/squanchy.png";
+import giraffe from "../Imagenes/giraffe.png";
+import gene from "../Imagenes/gene.png";
+import jessica from "../Imagenes/Jessica.png";
+import brad from "../Imagenes/brad.png";
+import ethan from "../Imagenes/ethan.png";
+import president from "../Imagenes/President.png";
+import tammy from "../Imagenes/tammy.png";
+import birdperson from "../Imagenes/Birdperson.png";
+import donna from "../Imagenes/Donna.png";
+import agency from "../Imagenes/agency.png";
+import mr from "../Imagenes/Mr. Beauregard.png";
+import king from "../Imagenes/King.png";
+import unity from "../Imagenes/Unity.png";
+import gazor from "../Imagenes/gazorpians.png";
+
+function Info(params) {
+  return (
+    <div className="fondo">
+      <center>
+        <h1>RICK AND MORTY</h1>
+      </center>
+
+      <div className="divicion">
+        <div className="con1">
+          <img className="marcontainer" src={rick} />
+          <center>
+            <div>
+              <center>
+                <h3>RICK SANCHEZ</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Masculino</p>
+              <p>Origen: Tierra (C-137)</p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="marcontainer" src={morty} />
+          <center>
+            <div>
+              <center>
+                <h3>MORTY SMITH</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Masculino</p>
+              <p>Origen: Tierra (C-137)</p>
+            </div>
+          </center>
+        </div>
+      </div>
+
+      <div className="be5" />
+
+      <div className="divicion">
+        <div className="con1">
+          <img className="marcontainer" src={summer} />
+          <center>
+            <div>
+              <center>
+                <h3>SUMMER SMITH</h3>
+              </center>
+              <p>Estado: Viva</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Femenina</p>
+              <p>Origen: Tierra (Dimensión de reemplazo)</p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="marcontainer" src={beth} />
+          <center>
+            <div>
+              <center>
+                <h3>BETH SMITH</h3>
+              </center>
+              <p>Estado: Viva</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Femenina</p>
+              <p>Origen: Tierra (Dimensión de reemplazo)</p>
+            </div>
+          </center>
+        </div>
+      </div>
+
+      <div className="be5" />
+
+      <div className="divicion">
+        <div className="con1">
+          <img className="marcontainer" src={giraffe} />
+          <center>
+            <div>
+              <center>
+                <h3>INVERSE GIRAFFE</h3>
+              </center>
+              <p>Estado: Fallecido</p>
+              <p>Especie: Parasito alienigena</p>
+              <p>Genero: Masculino</p>
+              <p>Origen: Imaginacion</p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="marcontainer" src={gene} />
+          <center>
+            <div>
+              <center>
+                <h3>GENE VAGINA</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Masculino</p>
+              <p>Origen: Tierra</p>
+            </div>
+          </center>
+        </div>
+      </div>
+      <div className="be5" />
+      <div className="divicion">
+        <div className="con1">
+          <img className="marcontainer" src={jerry} />
+          <center>
+            <div>
+              <center>
+                <h3>JERRY SMITH</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Masculino</p>
+              <p>Origen: Tierra </p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="marcontainer" src={squanchy} />
+          <center>
+            <div>
+              <center>
+                <h3>SQUANCHY</h3>
+              </center>
+              <p>Estado: Viva</p>
+              <p>Especie: Squanchies</p>
+              <p>Genero: Femenina</p>
+              <p>Origen: Planeta Squanch</p>
+            </div>
+          </center>
+        </div>
+      </div>
+
+      <div className="be5" />
+
+      <div className="divicion">
+        <div className="con1">
+          <img className="marcontainer" src={jessica} />
+          <center>
+            <div>
+              <center>
+                <h3>JESSICA</h3>
+              </center>
+              <p>Estado: Viva</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Femenina</p>
+              <p>Origen: Tierra </p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="marcontainer" src={brad} />
+          <center>
+            <div>
+              <center>
+                <h3>BRAD</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Masculino</p>
+              <p>Origen: Tierra </p>
+            </div>
+          </center>
+        </div>
+      </div>
+      <div className="be5" />
+
+      <div className="divicion">
+        <div className="con1">
+          <img className="marcontainer" src={ethan} />
+          <center>
+            <div>
+              <center>
+                <h3>ETHAN</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Masculino</p>
+              <p>Origen: Tierra</p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="marcontainer" src={president} />
+          <center>
+            <div>
+              <center>
+                <h3>THE PRESIDENT</h3>
+              </center>
+              <p>Estado: Vivo</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Masculino</p>
+              <p>Origen: Tierra</p>
+            </div>
+          </center>
+        </div>
+      </div>
+
+      <div className="be5" />
+
+      <div className="divicion">
+        <div className="con1">
+          <img className="marcontainer" src={tammy} />
+          <center>
+            <div>
+              <center>
+                <h3>TAMMY GUTERMAN</h3>
+              </center>
+              <p>Estado: Fallecida</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Femenina</p>
+              <p>Origen: Tierra</p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="marcontainer" src={birdperson} />
+          <center>
+            <div>
+              <center>
+                <h3>BIRDPERSON</h3>
+              </center>
+              <p>Estado: Fallecido</p>
+              <p>Especie: Persona pajaro</p>
+              <p>Genero: Masculino</p>
+              <p>Origen: Tierra</p>
+            </div>
+          </center>
+        </div>
+      </div>
+      <div className="be5" />
+
+      <div className="divicion">
+        <div className="con1">
+          <img className="marcontainer" src={donna} />
+          <center>
+            <div>
+              <center>
+                <h3>DONNA GUTERMAN</h3>
+              </center>
+              <p>Estado: Viva</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Femenina</p>
+              <p>Origen: Tierra</p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="marcontainer" src={agency} />
+          <center>
+            <div>
+              <center>
+                <h3>AGENCY DIRECTOR</h3>
+              </center>
+              <p>Estado: Fallecido</p>
+              <p>Especie: Humano</p>
+              <p>Genero: Masculino</p>
+              <p>Origen: Tierra (Dimensión de reemplazo)</p>
+            </div>
+          </center>
+        </div>
+      </div>
+      <div className="be5" />
+      <div className="divicion">
+        <div className="con1">
+          <img className="marcontainer" src={unity} />
+          <center>
+            <div>
+              <center>
+                <h3>UNITY</h3>
+              </center>
+              <p>Estado: Viva</p>
+              <p>Especie: Mente de la colmena</p>
+              <p>Genero: Femenina</p>
+              <p>Origen: Desconocido</p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="marcontainer" src={gazor} />
+          <center>
+            <div>
+              <center>
+                <h3>GAZORPIANS</h3>
+              </center>
+              <p>Estado: Existente</p>
+              <p>Especie: Humanoides</p>
+              <p>Genero: Masculino</p>
+              <p>Origen: Gazorpazorp</p>
+            </div>
+          </center>
+        </div>
+      </div>
+      <div className="be5" />
+
+      <div className="divicion">
+        <div className="con1">
+          <img className="marcontainer" src={mr} />
+          <center>
+            <div>
+              <center>
+                <h3>MR. BEAUREGARD</h3>
+              </center>
+              <p>Estado: Fallecido</p>
+              <p>Especie: parasito alienigena </p>
+              <p>Genero: Masculino</p>
+              <p>Origen: Tierra </p>
+            </div>
+          </center>
+        </div>
+
+        <div className="con1">
+          <img className="marcontainer" src={king} />
+          <center>
+            <div>
+              <center>
+                <h3>KING FLIPPY NIPS</h3>
+              </center>
+              <p>Estado: Desconocido</p>
+              <p>Especie: Plutoniano</p>
+              <p>Genero: Masculino</p>
+              <p>Origen: Plutón</p>
+            </div>
+          </center>
+        </div>
+      </div>
+      <div className="be5" />
+    </div>
+  );
+}
+export default Info;
